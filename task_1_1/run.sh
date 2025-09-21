d11309b8daffa8947bd35235fc4c0252527850c8
Maksim Vylegzhanin <maxvd2017@mail.ru>
2025-09-21 14:44:24 +0700

    Delete task_1_1 directory

--- task_1_1/run.sh
+++
@@ -1,6 +0,0 @@
-javac -d build/classes/java/main src/main/java/ru/nsu/vylegzhanin/HeapSort.java
-
-echo "Main-Class: ru.nsu.vylegzhanin.HeapSort" > MANIFEST.MF
-jar cfm build/libs/app.jar MANIFEST.MF -C build/classes/java/main .
-
-java -jar build/libs/app.jar
