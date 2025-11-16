108380393fae544907f503facfc53e7ae8ea528e
vylegzhaninn <m.vylegzhanin@g.nsu.ru>
2025-11-16 14:49:25 +0700

    Keep only .github and task_1_2_2 sources

--- task_1_2_1/src/main/java/ru/nsu/vylegzhanin/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.vylegzhanin;
-
-import ru.nsu.vylegzhanin.io.ConsoleInterface;
-
-public class Main {
-    public static void main(String[] args) {
-        new ConsoleInterface().run();
-    }
-}
